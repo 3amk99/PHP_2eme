5c520e4b60b5d4da5dd5b6f4b02126146e6d785c
Badreddinne <badrereste2003@gmail.com>
2026-02-24 12:07:41 +0000

    file

---
+++ Prototype/language.php
@@ -0,0 +1,114 @@
+<?php
+
+$text = [
+    "en" => [
+        "greeting" => "Hello",
+        "name_placeholder" => "Enter your name",
+        "choose_color" => "Choose color",
+        "save_button" => "Save",
+        "language_label" => "Choose language",
+        "reset_all" => "Reset all",
+        "en_name" => "English",
+        "fr_name" => "French",
+        "ar_name" => "Arabic",
+        "de_name" => "German",
+        "ta_name" => "Tamazight",
+        "ma_name" => "Chinese",
+        "ru_name" => "Russian"
+    ],
+    "fr" => [
+        "greeting" => "Bonjour",
+        "name_placeholder" => "Entrez votre nom",
+        "choose_color" => "Choisir la couleur",
+        "save_button" => "Enregistrer",
+        "language_label" => "Choisir la langue",
+        "reset_all" => "Tout réinitialiser",
+        "en_name" => "Anglais",
+        "fr_name" => "Français",
+        "ar_name" => "Arabe",
+        "de_name" => "Allemand",
+        "ta_name" => "Tamazight",
+        "ma_name" => "Chinois",
+        "ru_name" => "Russe"
+    ],
+    "ar" => [
+        "greeting" => "مرحبا",
+        "name_placeholder" => "أدخل اسمك",
+        "choose_color" => "اختر اللون",
+        "save_button" => "حفظ",
+        "language_label" => "اختر اللغة",
+        "reset_all" => "إعادة تعيين الكل",
+        "en_name" => "الإنجليزية",
+        "fr_name" => "الفرنسية",
+        "ar_name" => "العربية",
+        "de_name" => "الألمانية",
+        "ta_name" => "تامازيغت",
+        "ma_name" => "الصينية",
+        "ru_name" => "الروسية"
+    ],
+    "de" => [
+        "greeting" => "Hallo",
+        "name_placeholder" => "Geben Sie Ihren Namen ein",
+        "choose_color" => "Farbe wählen",
+        "save_button" => "Speichern",
+        "language_label" => "Sprache wählen",
+        "reset_all" => "Alles zurücksetzen",
+        "en_name" => "Englisch",
+        "fr_name" => "Französisch",
+        "ar_name" => "Arabisch",
+        "de_name" => "Deutsch",
+        "ta_name" => "Tamazight",
+        "ma_name" => "Chinesisch",
+        "ru_name" => "Russisch"
+    ],
+    "ta" => [
+        "greeting" => "ⴰⵣⵓⵍ",
+        "name_placeholder" => "ⵙⴽⴻⵙⴻⵎ ⵉⵙⴻⵎ-ⵉⵏⴻⴽ",
+        "choose_color" => "ⴼⵔⴻⵏ ⵉⵏⵉ",
+        "save_button" => "ⵙⴻⴽⵍⴻⵙ",
+        "language_label" => "ⴼⵔⴻⵏ ⵜⵓⵜⵍⴰⵢⵜ",
+        "character" => "ⴰⵎⵣⵣⵓⴷ",
+        "reset_all" => "ⴰⵙⴻⵎⵎⴻⵏ ⴰⴽⴽ",
+        "en_name" => "ⵉⵏⴳⵍⵉⵙⵉⵙ",
+        "fr_name" => "ⴼⵔⴰⵏⵙⵉⵙ",
+        "ar_name" => "ⵄⴻⵔⴱⵉⵙ",
+        "de_name" => "ⴷⵉⵎⴰⵏⴿ",
+        "ta_name" => "ⵜⴰⵎⴰⵣⵉⵖⵜ",
+        "ma_name" => "ⵛⵉⵏⴻⵙⵉⵙ",
+        "ru_name" => "ⵔⵓⵙⵙⵉⵙ"
+    ],
+    "ma" => [
+        "greeting" => "你好",
+        "name_placeholder" => "输入你的名字",
+        "choose_color" => "选择颜色",
+        "save_button" => "保存",
+        "language_label" => "选择语言",
+        "reset_all" => "全部重置",
+        "en_name" => "英语",
+        "fr_name" => "法语",
+        "ar_name" => "阿拉伯语",
+        "de_name" => "德语",
+        "ta_name" => "塔马齐格特语",
+        "ma_name" => "中文",
+        "ru_name" => "俄语"
+    ],
+    "ru" => [
+        "greeting" => "Привет",
+        "name_placeholder" => "Введите ваше имя",
+        "choose_color" => "Выберите цвет",
+        "save_button" => "Сохранить",
+        "language_label" => "Выберите язык",
+        "reset_all" => "Сбросить всё",
+        "en_name" => "Английский",
+        "fr_name" => "Французский",
+        "ar_name" => "Арабский",
+        "de_name" => "Немецкий",
+        "ta_name" => "Тамазигт",
+        "ma_name" => "Китайский",
+        "ru_name" => "Русский"
+    ]
+];
+
+
+
+?>
--- Prototype/php.php
+++ Prototype/php.php
@@ -1,9 +1,11 @@
 <?php
+include "language.php" ;
  if(isset($_POST['button_reset_all']))
  {
     setcookie("name_cookie", "", time() - 3600, "/");                
     setcookie("color_cookie", "#000000", time() - 3600, "/");   
-    setcookie("language_cookie", "en", time() - 3600, "/");  
+    setcookie("language_cookie", "en", time() - 3600, "/");
+    setcookie("date_cookie","", time() - 3600 ,"/");  
     header('Location: php.php');
     exit ;
  }
@@ -12,6 +14,7 @@
  {
     setcookie("name_cookie",$_POST['name'],time() + 3600, "/");
     setcookie("color_cookie",$_POST['color'],time() + 3600, "/");
+    setcookie("date_cookie",date(("y-m-d  h:i:s")), time() + 3600 ,"/");
     header("Location: php.php");
     exit ;
  }
@@ -23,120 +26,10 @@
    exit ;
  }
  $language = isset($_COOKIE['language_cookie']) ? $_COOKIE['language_cookie'] : "en"  ;
-  $name = isset($_COOKIE['name_cookie']) ? $_COOKIE['name_cookie'] : "" ;
+ $name = isset($_COOKIE['name_cookie']) ? $_COOKIE['name_cookie'] : "" ;
  $color = isset($_COOKIE['color_cookie']) ? $_COOKIE['color_cookie'] : "rgb(0, 0, 0)" ;
-$text = [
-    "en" => [
-        "greeting" => "Hello",
-        "name_placeholder" => "Enter your name",
-        "choose_color" => "Choose color",
-        "save_button" => "Save",
-        "language_label" => "Choose language",
-        "reset_all" => "Reset all",
-        "en_name" => "English",
-        "fr_name" => "French",
-        "ar_name" => "Arabic",
-        "de_name" => "German",
-        "ta_name" => "Tamazight",
-        "ma_name" => "Chinese",
-        "ru_name" => "Russian"
-    ],
-    "fr" => [
-        "greeting" => "Bonjour",
-        "name_placeholder" => "Entrez votre nom",
-        "choose_color" => "Choisir la couleur",
-        "save_button" => "Enregistrer",
-        "language_label" => "Choisir la langue",
-        "reset_all" => "Tout réinitialiser",
-        "en_name" => "Anglais",
-        "fr_name" => "Français",
-        "ar_name" => "Arabe",
-        "de_name" => "Allemand",
-        "ta_name" => "Tamazight",
-        "ma_name" => "Chinois",
-        "ru_name" => "Russe"
-    ],
-    "ar" => [
-        "greeting" => "مرحبا",
-        "name_placeholder" => "أدخل اسمك",
-        "choose_color" => "اختر اللون",
-        "save_button" => "حفظ",
-        "language_label" => "اختر اللغة",
-        "reset_all" => "إعادة تعيين الكل",
-        "en_name" => "الإنجليزية",
-        "fr_name" => "الفرنسية",
-        "ar_name" => "العربية",
-        "de_name" => "الألمانية",
-        "ta_name" => "تامازيغت",
-        "ma_name" => "الصينية",
-        "ru_name" => "الروسية"
-    ],
-    "de" => [
-        "greeting" => "Hallo",
-        "name_placeholder" => "Geben Sie Ihren Namen ein",
-        "choose_color" => "Farbe wählen",
-        "save_button" => "Speichern",
-        "language_label" => "Sprache wählen",
-        "reset_all" => "Alles zurücksetzen",
-        "en_name" => "Englisch",
-        "fr_name" => "Französisch",
-        "ar_name" => "Arabisch",
-        "de_name" => "Deutsch",
-        "ta_name" => "Tamazight",
-        "ma_name" => "Chinesisch",
-        "ru_name" => "Russisch"
-    ],
-    "ta" => [
-        "greeting" => "ⴰⵣⵓⵍ",
-        "name_placeholder" => "ⵙⴽⴻⵙⴻⵎ ⵉⵙⴻⵎ-ⵉⵏⴻⴽ",
-        "choose_color" => "ⴼⵔⴻⵏ ⵉⵏⵉ",
-        "save_button" => "ⵙⴻⴽⵍⴻⵙ",
-        "language_label" => "ⴼⵔⴻⵏ ⵜⵓⵜⵍⴰⵢⵜ",
-        "character" => "ⴰⵎⵣⵣⵓⴷ",
-        "reset_all" => "ⴰⵙⴻⵎⵎⴻⵏ ⴰⴽⴽ",
-        "en_name" => "ⵉⵏⴳⵍⵉⵙⵉⵙ",
-        "fr_name" => "ⴼⵔⴰⵏⵙⵉⵙ",
-        "ar_name" => "ⵄⴻⵔⴱⵉⵙ",
-        "de_name" => "ⴷⵉⵎⴰⵏⴿ",
-        "ta_name" => "ⵜⴰⵎⴰⵣⵉⵖⵜ",
-        "ma_name" => "ⵛⵉⵏⴻⵙⵉⵙ",
-        "ru_name" => "ⵔⵓⵙⵙⵉⵙ"
-    ],
-    "ma" => [
-        "greeting" => "你好",
-        "name_placeholder" => "输入你的名字",
-        "choose_color" => "选择颜色",
-        "save_button" => "保存",
-        "language_label" => "选择语言",
-        "reset_all" => "全部重置",
-        "en_name" => "英语",
-        "fr_name" => "法语",
-        "ar_name" => "阿拉伯语",
-        "de_name" => "德语",
-        "ta_name" => "塔马齐格特语",
-        "ma_name" => "中文",
-        "ru_name" => "俄语"
-    ],
-    "ru" => [
-        "greeting" => "Привет",
-        "name_placeholder" => "Введите ваше имя",
-        "choose_color" => "Выберите цвет",
-        "save_button" => "Сохранить",
-        "language_label" => "Выберите язык",
-        "reset_all" => "Сбросить всё",
-        "en_name" => "Английский",
-        "fr_name" => "Французский",
-        "ar_name" => "Арабский",
-        "de_name" => "Немецкий",
-        "ta_name" => "Тамазигт",
-        "ma_name" => "Китайский",
-        "ru_name" => "Русский"
-    ]
-];
-
-
  $solo_language = $text[$language] ;
-
+ $date = isset($_COOKIE['date_cookie']) ? $_COOKIE['date_cookie'] : date(("y-m-d  h:i:s")) ;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -153,8 +46,7 @@ $text = [
                     <div id="greeting">
                         <?php echo $solo_language['greeting'] ." ". ($name ? $name : "Guest") . "  👋" ?>
                         <?php 
-                              date_default_timezone_set("Africa/Casablanca");
-                              echo date("Y-m-d H:i:s");
+                             echo $date ;
                         ?>
                     </div>
 
@@ -173,7 +65,7 @@ $text = [
                     <div id="color_div">
                         <input type="color" id="color" name="color" value="<?php echo $color; ?>">
                     </div>
-                   <button type="submit" name="save_profile"> save kolchi</button>
+                   <button type="submit" name="save_profile"> <?php echo $solo_language['save_button']?></button>
                 </form>
 
 
